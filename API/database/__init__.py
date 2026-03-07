4f744a7eebbf0fe76844dc899ed3903711fa32d7
Dova-Khing <uwu068497@gmail.com>
2026-03-06 21:30:58 -0500

    fix:se arregla el problema que impedia que se mostrara los objetos por defecto

--- API/database/__init__.py
+++ API/database/__init__.py
@@ -6,8 +6,11 @@ Expone las funciones y objetos principales desde config.py
 """
 
 from .config import engine, SessionLocal, get_db, DATABASE_URL
-from API.src.entities.categoria import Categoria
-from API.src.entities.generos import Genero
+from ..src.crud.generos_crud import GeneroCRUD
+from ..src.crud.categoria_crud import CategoriaCRUD
+from sqlalchemy.orm import Session
+
+
 
 CATEGORIAS_DEFAULT = [
     "Película",
@@ -39,37 +42,15 @@ __all__ = ["engine",
 
 
 
-def init_categorias():
-    db = SessionLocal()
-    try:
-       for nombre in CATEGORIAS_DEFAULT:
-              existe =(
-                   db.query(Categoria)
-                     .filter(Categoria.nombre_categoria == nombre)
-                        .first()
-              )
-
-              if not existe:
-                    categoria = Categoria(nombre_categoria=nombre)
-                    db.add(categoria)
-                    db.commit()
-    finally:
-        db.close()
+def inicializar_categorias(db: Session):
+    crud = CategoriaCRUD(db)
+    for nombre in CATEGORIAS_DEFAULT:
+        if not crud.obtener_categoria_por_nombre(nombre):
+            crud.crear_categoria(nombre)
 
 
-def init_generos():
-    db = SessionLocal()
-    try:
-       for nombre in GENEROS_DEFAULT:
-              existe =(
-                   db.query(Genero)
-                     .filter(Genero.nombre_genero == nombre)
-                        .first()
-              )
-
-              if not existe:
-                    genero = Genero(nombre_genero=nombre)
-                    db.add(genero)
-                    db.commit()
-    finally:
-        db.close()
+def inicializar_generos(db: Session):
+    crud = GeneroCRUD(db)
+    for nombre in GENEROS_DEFAULT:
+        if not crud.obtener_genero_por_nombre(nombre):
+            crud.crear_genero(nombre)
